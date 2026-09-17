81ef512d93333479965e23ec65ef430b743f0c26
lienowen <lienowen@outlook.com>
2026-09-17 14:17:16 +0800

    Tighten gameplay camera framing

--- scripts/world_performance.gd
+++ scripts/world_performance.gd
@@ -10,9 +10,10 @@ func spawn_player() -> void:
 	player.fell_out.connect(on_fell)
 	player.heavy_impact.connect(on_impact)
 	var camera := Camera2D.new()
-	camera.position = Vector2(0, -250)
+	camera.position = Vector2(85, -235)
+	camera.zoom = Vector2(1.08, 1.08)
 	camera.position_smoothing_enabled = true
-	camera.position_smoothing_speed = 6
+	camera.position_smoothing_speed = 7
 	camera.limit_left = 0
 	camera.limit_right = 3550
 	camera.limit_top = 0
@@ -31,4 +32,3 @@ func on_impact(at: Vector2, speed: float) -> void:
 func complete_slice() -> void:
 	if player is PerformanceBearer: player.play_victory()
 	super()
-
